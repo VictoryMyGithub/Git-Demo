772d0479f840bbc3bc1e4a5a3d752319da789bdc
Viktoriia Havrylchuk <vik.vitaliivna@gmail.com>
2023-01-24 00:08:08 +0200

    My first commit

---
+++ FirstWorkGit.java
@@ -0,0 +1,21 @@
+@Test
+public void testAdd() {
+        int actualResult = calc.myMethodAdd(10, 5);
+        int expectedResult = 15;
+        assertEquals("10 + 5 must be 15", 15, actualResult);
+        }
+
+// TODO (*)
+//add tests for new methods (multiply, division, subtraction, square root, x^2)
+@Test
+public void methodForSubtraction() {
+        int actualResult = calc.myMethodSubtraction(10, 5);
+        int expectedResult = 5;
+        assertEquals("10 - 5 must be 5", 5, actualResult);
+        }
+
+@Test
+public void methodForMultiplication() {
+        int actualResult = calc.myMethodMultiplication(10, 5);
+        int expectedResult = 50;
+        assertEquals("10 * 5 must be 50", 50, actualResult);
