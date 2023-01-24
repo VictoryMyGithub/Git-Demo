110b099c5674129a9e5f6f9380b229d556baaea2
VictoryMyGithub <123422682+VictoryMyGithub@users.noreply.github.com>
2023-01-24 16:33:50 +0200

    Add new commit

--- FirstWorkGit.java
+++ FirstWorkGit.java
@@ -20,10 +20,4 @@ public void methodForMultiplication() {
         int expectedResult = 50;
         assertEquals("10 * 5 must be 50", 50, actualResult);
 
-        public void methodForMultiplication() {
-        int actualResult = calc.myMethodMultiplication(10, 5);
-        int expectedResult = 50;
-        assertEquals("10 * 5 must be 50", 50, actualResult);
-
-
-        Fix bug test newBrench
+        
